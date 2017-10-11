c9ea763745db992845998d399a1778b2babedce2
Sergio Chin-Sue <s.chinsue@gmail.com>
2017-10-11 14:21:03 +0100

    Push notifications demo

--- App.js
+++ App.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Permissions, Notifications } from 'expo';
+
+const PUSH_ENDPOINT = 'https://your-server.com/users/push-token';
+
+async function registerForPushNotificationsAsync() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== 'granted') {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== 'granted') {
+    return;
+  }
+
+  // Get the token that uniquely identifies this device
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  console.log(token)
+}
 
 export default class App extends React.Component {
+  state = {
+    notification: {},
+  };
+
+  componentWillMount() {
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    this._notificationSubscription = Notifications.addListener(this._handleNotification)
+  }
+
+  _handleNotification = (notification) => {
+    this.setState({
+      notification
+    })
+    console.log(this.state.notification)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={registerForPushNotificationsAsync}
+          title="Console Log Device Token"
+          color="#841584"
+          accessibilityLabel="Console log device token"
+        />
       </View>
     );
   }
@@ -19,5 +71,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 50
   },
 });
